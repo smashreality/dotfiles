17b52570edb770504898c64c2dd5b9f614483395
Barry Morwood <bmorwood@users.noreply.github.com>
2017-01-10 14:10:26 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,60 +3,58 @@
 cd $HOME
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
- #   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-
-# this will intialize the download of xcode command line tools  (FIX: make this an actual command to install CLI tools)
-git --version
-
-# git contrib files
-# https://github.com/git/git/tree/master/contrib
+xcode-select --install
 
 # add git aliases
+
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 
-# install git auto complete script and prompt
-curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-echo "source ~/git-completion.bash" >> .bash_profile
-echo "source ~/git-prompt.sh" >> .bash_profile
-echo "export GIT_PS1_SHOWDIRTYSTATE=1" >> .bash_profile
-# export PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
+# install brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# install and update homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap homebrew/versions
+brew update
+brew upgrade —all
+# brew cleanup
+# brew prune
 
-brew doctor
-brew update && brew upgrade --all && brew cleanup && brew prune
-
-brew install vim
+brew install dockutil
 brew install node
-brew install fortune
-brew install cowsay
-brew install grip
+brew install vim
 
-# install Cask to install applications
-# search the applications here https://caskroom.github.io/search
-brew install cask 
+# install brew cask
+brew tap caskroom/cask
+brew install brew-cask
 
-# install applications using Cask
-#brew cask install caffeine - set always awake default in .macos
+# install applications using cask
 brew cask install 1password
-brew cask install firefox
+brew cask install adobe-creative-cloud
 brew cask install evernote
+brew cask install firefox
 brew cask install google-chrome
+# brew cask install java
 brew cask install omnigraffle
 brew cask install slack
 brew cask install spectacle
-brew cask install virtualbox
-brew cask install java
-brew cask install adobe-creative-cloud
-brew cask install vagrant
-# brew install gpg
+# brew cask install virtualbox
 
-# install docker for mac (opt)
+open '/usr/local/Caskroom/adobe-creative-cloud/latest/Creative Cloud Installer.app'
 
+# dock
+
+# Set the icon size of Dock items to 36 pixels
+
+defaults write com.apple.dock tilesize -int 36
+
+
+# setup dock using “dockutil”
+
+dockutil --no-restart --remove all
+
+dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Firefox.app"
+
+killall Dock
